5af050d833a8eea64ee6af2f4d8d753a603998a8
kzwkt <9437935+kzwkt@users.noreply.github.com>
2024-06-30 07:27:32 +0000

    Add files via upload

--- .bin/playerctl.sh
+++ .bin/playerctl.sh
@@ -1,43 +1,28 @@
 #!/usr/bin/env bash
-mpris() {
-    local command="$1"
-    # Find all MPRIS-compatible media player
-    local players=$(busctl --user list | grep 'org.mpris.MediaPlayer2' | awk '{print $1}')
+[ -z "$1" ] && { echo "Please provide a command (next, play-pause, previous, stop)."; exit 1; }
 
-if [ -z "$command" ]; then
-        echo $players
-        echo "Please provide a command next,play-pause,previous, stop)."
-        return
-    fi
-
-if pgrep -x "mpv" > /dev/null; then
-        case "$command" in
-            "Next")
-                echo "script-binding uosc/next" | socat - /tmp/mpv
-                return
-                ;;
-            "Previous")
-                echo "script-binding uosc/prev" | socat - /tmp/mpv
-                return
-                ;;
-            "PlayPause")
-                 echo "cycle pause" | socat - /tmp/mpv
-        esac
-    fi
-
-    # Send the corresponding command to each found mpris instance
-    for instance in $players; do
-        busctl --user call "$instance" /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player "$command"
-    done
+pgrep -x "mpv" > /dev/null && {
+mpc() { echo "$1" | socat - /tmp/mpv; exit 0; }
+case "$1" in
+"next") mpc "script-binding uosc/next" ;;
+"previous") mpc "script-binding uosc/prev" ;;
+"play-pause") mpc "cycle pause"  ;;
+*) exit 1 ;;
+esac
 }
 
 
+mpris() {
+players=$(busctl --user list | grep 'org.mpris.MediaPlayer2' | awk '{print $1}')
+for x in $players; do
+busctl --user call "$x" /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player "$1"
+done
+}
+
 case "$1" in
-	"next") mpris "Next" ;;
-        "play-pause") mpris "PlayPause" ;;
-        "previous") mpris "Previous" ;;
-        "stop") mpris "Stop" ;;
-        *) mpris ;;
-    esac
-
-
+"next") mpris "Next" ;;
+"play-pause") mpris "PlayPause" ;;
+"previous") mpris "Previous" ;;
+"stop") mpris "Stop" ;;
+*) exit 1  ;;
+esac
