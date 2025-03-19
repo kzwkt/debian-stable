1f410a9cc1d89ea200c91690c5b89a582b2cdbc9
kzwkt <9437935+kzwkt@users.noreply.github.com>
2025-03-19 16:35:33 +0545

    Update playerctl.sh

--- .bin/playerctl.sh
+++ .bin/playerctl.sh
@@ -2,7 +2,7 @@
 [ -z "$1" ] && { echo "Please provide a command (next, play-pause, previous, stop)."; exit 1; }
 
 pgrep -x "mpv" > /dev/null && {
-mpc() { echo "$1" | socat - /tmp/mpv; exit 0; }
+mpc() { echo "$1" |   nc -U -N /tmp/mpv ; exit 0; }
 case "$1" in
 "next") mpc "script-binding uosc/next" ;;
 "previous") mpc "script-binding uosc/prev" ;;
@@ -11,13 +11,28 @@ case "$1" in
 esac
 }
 
-
 mpris() {
+
+if command -v busctl >/dev/null; then
 players=$(busctl --user list | grep 'org.mpris.MediaPlayer2' | awk '{print $1}')
+runn(){
+busctl --user call "$1" /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player "$2"
+}
+elif command -v dbus-send >/dev/null; then
+players=$(dbus-send --print-reply --dest=org.freedesktop.DBus /org/freedesktop/DBus org.freedesktop.DBus.ListNames | grep 'org.mpris.MediaPlayer2' | awk -F '"' '{print $2}')
+runn(){
+dbus-send --session --dest="$1" --type=method_call /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player."$2"
+}
+else
+echo "Neither busctl nor dbus-send found." >&2
+return 1
+fi
+
 for x in $players; do
-busctl --user call "$x" /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player "$1"
+runn "$x" "$1"
 done
 }
+
 
 case "$1" in
 "next") mpris "Next" ;;
